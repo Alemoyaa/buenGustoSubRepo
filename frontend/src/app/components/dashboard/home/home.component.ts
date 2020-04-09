a013625d1785605fb6cac617f432a01cd9fee30b
Fernando Moya <fmoya97.fm@gmail.com>
2020-04-09 02:36:01 -0300

    SaqueHome

--- frontend/src/app/components/dashboard/home/home.component.ts
+++ frontend/src/app/components/dashboard/home/home.component.ts
@@ -7,49 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   
-  public posts: {
-    id: string;
-    titlePost: string;
-    contentPost: string;
-    imagePost: string;
-  }[] = [
-    {
-      id: '1',
-      titlePost: 'PostOne',
-      contentPost: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam`,
-
-      imagePost: 'https://i.blogs.es/9c5ee0/pizza-cebolla/1366_2000.jpg'
-    },
-    {
-      id: '2',
-      titlePost: 'PostTwo',
-      contentPost: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam`,
-
-      imagePost: 'https://media-cdn.tripadvisor.com/media/photo-s/0e/0a/27/05/pizza-especial-salsa.jpg'
-    },
-    {
-      id: '3',
-      titlePost: 'Post 3',
-      contentPost: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam`,
-
-      imagePost: 'https://image.freepik.com/foto-gratis/rebanada-cortada-clasica-pizza-pepperoni-rollos-pimiento-verde_114579-1963.jpg'
-    },
-    {
-      id: '4',
-      titlePost: 'Post4',
-      contentPost: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam`,
-
-      imagePost: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'
-    }
-  ];
   constructor() { }
 
   ngOnInit(): void {
