ee31c2299f4128b9b955058ffb138a2062c220cf
Fernando Moya <fmoya97.fm@gmail.com>
2020-04-11 14:15:26 -0300

    nuevo front- estructura

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/admin/login/login.component';
+import { RegisterComponent } from './components/admin/register/register.component';
+import { HomeComponent } from './components/cliente/home/home.component';
+import { NavbarComponent } from './components/cliente/navbar/navbar.component';
+import { NotfoundComponent } from './components/cliente/notfound/notfound.component';
+import { UserProfileComponent } from './components/cliente/user-profile/user-profile.component';
+import { CatalogoComponent } from './components/cliente/catalogo/catalogo.component';
+import { OfertasComponent } from './components/cliente/ofertas/ofertas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    UserProfileComponent,
+    CatalogoComponent,
+    OfertasComponent
   ],
   imports: [
     BrowserModule,
