f0c0f75e21fd72c9d6aefea9318e846ed1900da5
Fernando Moya <fmoya97.fm@gmail.com>
2020-04-11 16:19:20 -0300

    rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { OfertasComponent } from './components/cliente/ofertas/ofertas.component';
+import { NotfoundComponent } from './components/cliente/notfound/notfound.component';
+import { NavbarComponent } from './components/cliente/navbar/navbar.component';
+import { CatalogoComponent } from './components/cliente/catalogo/catalogo.component';
+import { HomeComponent } from './components/cliente/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'catalogo', component: CatalogoComponent },
+  { path:'navbar', component: NavbarComponent },
+  { path:'ofertas', component: OfertasComponent },
+  { path:'**', component: NotfoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
