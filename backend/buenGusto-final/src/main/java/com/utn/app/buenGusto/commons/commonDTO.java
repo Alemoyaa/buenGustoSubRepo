eb9210a0febe4821b78840b08a6fdce0ed7c42ea
alemoya <alejandro5320@gmail.com>
2020-04-12 03:11:05 -0300

    backend v0.3

---
+++ backend/buenGusto-final/src/main/java/com/utn/app/buenGusto/commons/commonDTO.java
@@ -0,0 +1,13 @@
+package com.utn.app.buenGusto.commons;
+
+public class commonDTO {
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
