8e6a12d598ecc4013a269d281e385441993d60fb
alemoya <alejandro5320@gmail.com>
2020-04-16 21:11:57 -0300

    Pdf editado

--- frontend/src/app/components/cliente/user-profile/user-profile.component.ts
+++ frontend/src/app/components/cliente/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import jsPDF from 'jspdf';
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-user-profile',
@@ -13,11 +14,22 @@ export class UserProfileComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  crearFacturaPDF(){
-    var doc = new jsPDF()
 
-    doc.text('Hello world!', 10, 10)
-    doc.save('a4.pdf')
+  crearFacturaPDF(){
+    var data = document.getElementById('page-top');
+
+    html2canvas(data).then(canvas => {
+      // Configuracion necesaria para la foto
+      var imgWidth = 208;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+
+      const contentDataURL = canvas.toDataURL('image/png') //Creamos una imagen la cual despues convertimos
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 va a ser el tamaño del PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('factura.pdf'); // Generamos el PDF
+    });
+
   }
 
 }
