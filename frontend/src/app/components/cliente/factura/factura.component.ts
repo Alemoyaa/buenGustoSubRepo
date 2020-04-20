e286e06e1b8429d85b88e9ff801980040910869d
salinas38m <salinas38m@gmail.com>
2020-04-20 16:35:26 -0300

    diseño de factura

--- frontend/src/app/components/cliente/factura/factura.component.ts
+++ frontend/src/app/components/cliente/factura/factura.component.ts
@@ -8,27 +8,26 @@ import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-factura',
   templateUrl: './factura.component.html',
-  styleUrls: ['./factura.component.css']
+  styleUrls: ['./factura.component.css'],
 })
 export class FacturaComponent implements OnInit {
-
-  factura :Factura={
+  factura: Factura = {
     id: null,
     fecha: null,
     formaPago: null,
     montoDescuento: null,
     nroTarjeta: null,
     numero: null,
-    total: null
+    total: null,
   };
 
   constructor(
     private rutaActiva: ActivatedRoute,
     private servicio: FacturaService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.rutaActiva.params.subscribe(data => {
+    this.rutaActiva.params.subscribe((data) => {
       if (data.id !== '0') {
         this.getOne(data.id);
       }
@@ -41,21 +40,19 @@ export class FacturaComponent implements OnInit {
     });
   }
 
-  crearFacturaPDF(){
+  crearFacturaPDF() {
     var data = document.getElementById('facturaPDF');
 
-    html2canvas(data).then(canvas => {
+    html2canvas(data).then((canvas) => {
       // Configuracion necesaria para la foto
       var imgWidth = 208;
-      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-      const contentDataURL = canvas.toDataURL('image/png') //Creamos una imagen la cual despues convertimos
+      const contentDataURL = canvas.toDataURL('image/png'); //Creamos una imagen la cual despues convertimos
       let pdf = new jsPDF('p', 'mm', 'a4'); // A4 va a ser el tamaño del PDF
       var position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
       pdf.save('factura.pdf'); // Generamos el PDF
     });
-
   }
-
 }
--- frontend/src/app/components/cliente/user-profile/user-profile.component.ts
+++ frontend/src/app/components/cliente/user-profile/user-profile.component.ts
@@ -4,16 +4,15 @@ import { UserProfileService } from './../../../services/serviciosCliente/userPro
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
-
   cliente: Cliente = {
     id: 0,
+    uidFirebase: '',
     nombre: '',
     apellido: '',
     telefono: null,
@@ -23,41 +22,35 @@ export class UserProfileComponent implements OnInit {
       id: 0,
       calle: '',
       localidad: '',
-      numero: null
-    }
+      numero: null,
+    },
   };
-
-
 
   constructor(
     private service: UserProfileService,
     private rutaActiva: ActivatedRoute,
     private serviceLogin: LoginService
-
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     //this.rutaActiva.params.subscribe(data => { Esto anda, falta router el cliente y sacarle la foto mersa
-     //if (data.id !== '0') {
-        //this.getOne(data.id);
-      //}
+    //if (data.id !== '0') {
+    //this.getOne(data.id);
+    //}
     //});
-this.datosUser();
+    this.datosUser();
   }
 
-  datosUser(){
-    return this.serviceLogin.datosGoogle(this.cliente);//me traigo los datos de google desde el service
+  datosUser() {
+    return this.serviceLogin.datosGoogle(this.cliente); //me traigo los datos de google desde el service
   }
 
-
-
-  async getOne(id: number){
-    await this.service.getOne(id).subscribe((data)=>{
+  async getOne(id: number) {
+    await this.service.getOne(id).subscribe((data) => {
       console.log(data);
       this.cliente = data;
     });
   }
-
 
   /*
   HTML DE FACTURA:
@@ -79,5 +72,4 @@ this.datosUser();
     });
 
   }*/
-
 }
