65afe924348970920deadc421e3de4012d5472b0
ramiro2021 <ramiroamurua@hotmail.com>
2020-04-21 14:51:39 -0300

    componente usuarios para administrador

--- frontend/src/app/components/admin/gerente/dashboard-gerente/dashboard-gerente.component.ts
+++ frontend/src/app/components/admin/gerente/dashboard-gerente/dashboard-gerente.component.ts
@@ -13,23 +13,29 @@ export class DashboardGerenteComponent implements OnInit {
   tableroPizzaCrud = true;
   tableroStock = false;
   tableroEstadisticas = false;
+  tableroUsuario = false;
 
   clearBoards() {
     this.tableroPizzaCrud = false;
     this.tableroStock = false;
     this.tableroEstadisticas = false;
+    this.tableroUsuario = false;
   }
 
   setBoard(board) {
     this.clearBoards();
-    if (board == 'tableroPizzaCrud') {
+    if (board === 'tableroPizzaCrud') {
       this.tableroPizzaCrud = true;
     }
-    if (board == 'tableroStock') {
+    if (board === 'tableroStock') {
       this.tableroStock = true;
     }
-    if (board == 'tableroEstadisticas') {
+    if (board === 'tableroEstadisticas') {
       this.tableroEstadisticas = true;
+    }
+
+    if (board === 'tableroUsuario') {
+      this.tableroUsuario = true;
     }
   }
 }
