0e1d9a8bf0dfdf891d7ce33620513c814d949594
Moya Fernando <fmoya97.fm@gmail.com>
2020-04-23 20:51:31 -0300

    filtros y paginacion sin funcionar de catalogo

--- frontend/src/app/components/cliente/catalogo/catalogo.component.ts
+++ frontend/src/app/components/cliente/catalogo/catalogo.component.ts
@@ -1,20 +1,25 @@
 import { ArticuloManufacturadoService } from '../../../services/serviciosCliente/articuloManufacturadoServices/articuloManufacturado.service';
 import { ArticuloManufacturado } from 'src/app/entidades/ArticuloManufacturado';
-
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
-  styleUrls: ['./catalogo.component.css']
+  styleUrls: ['./catalogo.component.css'],
+
 })
 export class CatalogoComponent implements OnInit {
 
   articulosManufacturados: ArticuloManufacturado[] = [];
+  rubro = ['Todo', 'Pizza', 'Lomo'];
 
-  constructor(public service: ArticuloManufacturadoService) { }
+
+  constructor(public service: ArticuloManufacturadoService) {
+    ;
+  }
 
   ngOnInit(): void {
+
     this.getAll();
   }
 
@@ -24,4 +29,31 @@ export class CatalogoComponent implements OnInit {
       console.log(this.articulosManufacturados);
     });
   }
+
+
+  getFiltro(filtro: string){
+    this.service.getAll().subscribe((data)=>{
+      this.articulosManufacturados = data,{
+        query:{
+          orderByChild: 'rubroGeneral',
+          equalTo: filtro
+        }
+      }
+      console.log(this.articulosManufacturados);
+    });
+    return this.articulosManufacturados;
+  }
+
+  onSelect(event){
+    let query=null;
+    if(event.value== "Todos"){
+      query= this.getAll();
+    }else{
+      query = this.getFiltro(event.value);
+      query.subscribe(rubro =>{
+        this.articulosManufacturados= this.articulosManufacturados;
+      })
+    }
+  }
+
 }
