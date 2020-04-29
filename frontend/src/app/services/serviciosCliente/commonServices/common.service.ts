47610ee245454b8d8f066d77e8fdcd489a09c693
salinas38m <salinas38m@gmail.com>
2020-04-29 02:53:47 -0300

    Estadisticas, chart genérico

--- frontend/src/app/services/serviciosCliente/commonServices/common.service.ts
+++ frontend/src/app/services/serviciosCliente/commonServices/common.service.ts
@@ -4,57 +4,52 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommonService<Entity> {
+  protected _url: string;
 
-  protected _url:string;
+  constructor(protected http: HttpClient) {}
 
-  constructor(protected http:HttpClient) {
+  getAll(): Observable<Entity[]> {
+    return this.http
+      .get<Entity[]>(this._url)
+      .pipe(catchError(this.handleError));
   }
 
-  getAll() : Observable<Entity[]>{
-    return this.http.get<Entity[]>(this._url)
-    .pipe(catchError(this.handleError));
+  getOne(id: number): Observable<Entity> {
+    return this.http
+      .get<Entity>(this._url + id)
+      .pipe(catchError(this.handleError));
   }
 
-  getOne(id:number) : Observable<Entity>{
-    return this.http.get<Entity>(this._url+id)
-    .pipe(catchError(this.handleError));
+  post(entidad: Entity): Observable<Entity> {
+    return this.http
+      .post<Entity>(this._url, entidad)
+      .pipe(catchError(this.handleError));
   }
 
-  post(entidad:Entity) : Observable<Entity>{
-    return this.http.post<Entity>(this._url, entidad)
-    .pipe(catchError(this.handleError));
+  put(id: number, entidad: Entity): Observable<Entity> {
+    return this.http
+      .put<Entity>(this._url + id, entidad)
+      .pipe(catchError(this.handleError));
   }
 
-  put(id:number, entidad:Entity) : Observable<Entity>{
-    return this.http.put<Entity>(this._url+id, entidad)
-    .pipe(catchError(this.handleError));
+  delete(id: number): Observable<any> {
+    return this.http.delete(this._url + id).pipe(catchError(this.handleError));
   }
-
-  delete(id:number) : Observable<any>{
-    return this.http.delete(this._url+id)
-    .pipe(catchError(this.handleError));
-  }
-
 
   handleError(error) {
     let errorMessage = 'Error manejado';
     if (error.error instanceof ErrorEvent) {
-
       //error de cliente
       errorMessage = `Error: ${error.error.mensage}`;
-
     } else {
-
       //error de server
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}\nError de Servidor`;
     }
 
-    alert(errorMessage);
+    console.log(errorMessage);
     return throwError(errorMessage);
-
   }
-
 }
