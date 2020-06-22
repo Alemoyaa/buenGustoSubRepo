56f9fa13e8756370da0af03ae6ea83144e78bd87
Luis Aruta <luis.aruta@alumnos.frm.utn.edu.ar>
2020-06-22 18:50:30 -0300

    Se elimino la tabla de Detalle Factura por desuso

--- backend/buenGusto-final/src/main/java/com/utn/app/buenGusto/detalleFactura/DetalleFacturaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utn.app.buenGusto.detalleFactura;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetalleFacturaRepository extends JpaRepository<DetalleFacturaEntity, Long> {
-
-}
