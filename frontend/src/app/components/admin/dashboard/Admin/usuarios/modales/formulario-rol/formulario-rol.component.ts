2703e8387d166303a34c45ea7661690e48b1f8a2
ramiro2021 <ramiroamurua@hotmail.com>
2020-06-22 12:12:30 -0300

    precargar usuario con todos sus datos/ falta arreglar error de seteado

--- frontend/src/app/components/admin/dashboard/Admin/usuarios/modales/formulario-rol/formulario-rol.component.ts
+++ frontend/src/app/components/admin/dashboard/Admin/usuarios/modales/formulario-rol/formulario-rol.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cliente } from 'src/app/entidades/Cliente';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { ClienteService } from 'src/app/services/serviciosCliente/clienteServices/cliente.service';
+import { RolService } from 'src/app/services/serviciosCliente/rolServices/rol.service';
+import { AlertsService } from 'src/app/services/alertServices/alerts.service';
+import { Rol } from 'src/app/entidades/Rol';
+
 
 @Component({
   selector: 'app-formulario-rol',
@@ -6,10 +18,136 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-rol.component.css']
 })
 export class FormularioRolComponent implements OnInit {
+  cliente: Cliente;
+  formularioPersona: FormGroup;
+  rol: Rol[] = new Array<Rol>();
+  rolSeleccionado: Rol;
 
-  constructor() { }
+  @Input() set clienteSeleccionado(cliente) {
+    this.crearFormulario();
+    if (cliente) {
+      this.cliente = cliente;
+      this.formularioPersona.setValue({
+        id: cliente.id,
+        nombre: cliente.nombre,
+        apellido: cliente.apellido,
+        telefono: cliente.telefono,
+
+        usuario: {
+          id: cliente.usuario.id,
+          email: cliente.usuario.email,
+          rol: {
+            id: cliente.usuario.rol.id,
+            nombreRol: cliente.usuario.rol.nombreRol
+          }
+
+        },
+
+        domicilio: {
+          id: cliente.domicilio.id,
+          calle: cliente.domicilio.calle,
+          nroDepartamento: cliente.domicilio.nroDepartamento,
+          numero: cliente.domicilio.numero,
+          piso: cliente.domicilio.piso,
+          aclaracion: cliente.domicilio.aclaracion,
+          localidad: {
+            id: cliente.domicilio.localidad.id,
+            nombre: cliente.domicilio.localidad.nombre,
+            provincia: {
+              id: cliente.domicilio.localidad.provincia.id,
+              nombre: cliente.domicilio.localidad.provincia.nombre,
+              pais: {
+                id: cliente.domicilio.localidad.provincia.pais.id,
+                nombre: cliente.domicilio.localidad.provincia.pais.nombre
+              }
+            }
+          }
+        },
+
+
+      });
+    }
+
+
+
+
+
+  }
+  constructor(
+    private fb: FormBuilder,
+    private clienteService: ClienteService,
+    private rolService: RolService,
+    private alerts: AlertsService) { }
 
   ngOnInit(): void {
+    this.crearFormulario();
+    this.traerRoles();
+
+  }
+
+
+  crearFormulario() {
+    // creo el formulario todo por default vacio y le asigno que sea disabled para q no se puedan editar
+    // solo se podra editar el rol de la persona
+    this.formularioPersona = this.fb.group({
+      id: 0,
+      nombre: '',
+      apellido: '',
+      telefono: '',
+
+      domicilio: this.fb.group({
+        id: 0,
+        calle: '',
+        nroDepartamento: 0,
+        numero: 0,
+        piso: 0,
+        aclaracion: '',
+        localidad: this.fb.group({
+          id: 0,
+          nombre: '',
+          provincia: this.fb.group({
+            id: 0,
+            nombre: '',
+            pais: this.fb.group({
+              id: 0,
+              nombre: ''
+            })
+          })
+        })
+      }),
+      usuario: this.fb.group({
+        id: 0,
+        email: '',
+        rol: this.fb.group({
+          id: 0,
+          nombreRol: ''
+        })
+      }),
+    });
+  }
+
+  actualizar() {
+    console.log(this.formularioPersona.value);
+
+  }
+
+  seleccionarRol(id: number) {
+    console.log('seleccionar rol id parametro :', id);
+
+    if (id != null) {
+      this.rolService.getOne(id).subscribe((rol) => {
+        this.rolSeleccionado = rol;
+
+        console.log(this.rolSeleccionado);
+      });
+    }
+  }
+
+  traerRoles() {
+    this.rolService.getAll().subscribe((roles) => {
+      this.rol = roles;
+      console.log('Traer Roles() :', this.rol);
+    });
   }
 
 }
--- frontend/src/app/components/admin/dashboard/Admin/usuarios/usuarios.component.ts
+++ frontend/src/app/components/admin/dashboard/Admin/usuarios/usuarios.component.ts
@@ -20,24 +20,24 @@ import { Domicilio } from 'src/app/entidades/Domicilio';
   styleUrls: ['./usuarios.component.css'],
 })
 export class UsuariosComponent implements OnInit {
-  formularioPersona: FormGroup;
+ 
   usuarios: Cliente[] = new Array<Cliente>();
-  rol: Rol[] = new Array<Rol>();
-  rolSeleccionado: Rol;
-  idUsuario: number;
+
+  
+
   domicilio: Domicilio = new Domicilio();
+  cliente: Cliente = new Cliente();
 
   constructor(
-    private fb: FormBuilder,
+   
     private clienteService: ClienteService,
-    private rolService: RolService,
+  
     private alerts: AlertsService
   ) { }
 
   ngOnInit(): void {
     this.traerDatos();
-    this.crearFormulario();
-    this.traerRoles();
+   
 
     this.alerts.mensajeSuccess('Bienvenido a la seccion Usuario', 'Aqui usted podra asignar el rol a cada usuario registrado en la pagina');
   }
@@ -54,45 +54,7 @@ export class UsuariosComponent implements OnInit {
     });
   }
 
-  crearFormulario() {
-    // creo el formulario todo por default vacio y le asigno que sea disabled para q no se puedan editar
-    // solo se podra editar el rol de la persona
-    this.formularioPersona = this.fb.group({
-      nombre: '',
-      apellido: '',
-      telefono: '',
-
-      domicilio: this.fb.group({
-        id: 0,
-        calle: '',
-        nroDepartamento: 0,
-        numero: 0,
-        piso: 0,
-        aclaracion: '',
-        localidad: this.fb.group({
-          id: 0,
-          nombre: '',
-          provincia: this.fb.group({
-            id: 0,
-            nombre: '',
-            pais: this.fb.group({
-              id: 0,
-              nombre: ''
-            })
-          })
-        })
-      }),
-      usuario: this.fb.group({
-        id: 0,
-        email: '',
-        rol: this.fb.group({
-          id: 0,
-          nombreRol: ''
-        })
-      }),
-    });
-  }
-
+ 
   mostrarDomicilio(cliente:Cliente){
     this.domicilio = cliente.domicilio;
     console.log(this.domicilio);
@@ -100,86 +62,31 @@ export class UsuariosComponent implements OnInit {
 
   // pre cargo los datos en el formulario de el usuario seleccionnado para editar el rol
   preCargarDatosFormulario(cliente: Cliente) {
-
-   
-    // this.formularioPersona.setValue({
-    //   nombre: cliente.nombre,
-    //   apellido: cliente.apellido,
-    //   telefono: cliente.telefono,
-      
-    //   domicilio: this.fb.group({
-    //     id: cliente.domicilio.id,
-    //     calle: cliente.domicilio.calle,
-    //     nroDepartamento: cliente.domicilio.nroDepartamento,
-    //     numero: cliente.domicilio.numero,
-    //     piso: cliente.domicilio.piso,
-    //     aclaracion: cliente.domicilio.aclaracion,
-    //     localidad: this.fb.group({
-    //       id: cliente.domicilio.localidad.id,
-    //       nombre: cliente.domicilio.localidad.nombre,
-    //       provincia: this.fb.group({
-    //         id: cliente.domicilio.localidad.provincia.id,
-    //         nombre: cliente.domicilio.localidad.provincia.nombre,
-    //         pais: this.fb.group({
-    //           id: cliente.domicilio.localidad.provincia.pais.id,
-    //           nombre: cliente.domicilio.localidad.provincia.pais.nombre
-    //         })
-    //       })
-    //     })
-    //   }),
-    //   usuario: this.fb.group({
-    //     id: 0,
-    //     email: '',
-    //     rol: this.fb.group({
-    //       id: 0,
-    //       nombreRol: ''
-    //     })
-    //     //descripcion: '',
-    //   }),
-
-    // });
+    this.cliente = cliente;
+   console.log(this.cliente);
   }
 
-  // Traer todos los posibles roles para mostrarlos en el html
-  traerRoles() {
-    this.rolService.getAll().subscribe((roles) => {
-      this.rol = roles;
-      console.log('Traer Roles() :', this.rol);
-    });
-  }
 
-  // seleccionamos rol en html
-  seleccionarRol(id: number) {
-    console.log('seleccionar rol id parametro :', id);
-
-    if (id != null) {
-      this.rolService.getOne(id).subscribe((rol) => {
-        this.rolSeleccionado = rol;
-
-        console.log(this.rolSeleccionado);
-      });
-    }
-  }
 
   // post a la base de datos con el cliente y su rol
-  actualizarRol() {
+  // actualizarRol() {
     // this.clienteService.put(this.idCliente, this.formularioPersona.value);
-    console.log('id cliente actualziarRol() ', this.idUsuario);
-    console.log(
-      'formulario value actualziarRol() ',
-      this.formularioPersona.value
-    );
-  }
+  //   console.log('id cliente actualziarRol() ', this.idUsuario);
+  //   console.log(
+  //     'formulario value actualziarRol() ',
+  //     this.formularioPersona.value
+  //   );
+  // }
 
   // se pide confirmacion de la eliminacion de el usuario y se elimina segun el id recibido por parametro
-  eliminar(id: number) {
-    const opcion = confirm('¿Esta seguro que desea eliminar?');
-    if (opcion) {
-      this.clienteService.delete(id).subscribe((data) => {
-        alert('Registro eliminado');
-        this.formularioPersona.reset();
-      });
-    } else {
-    }
-  }
+  // eliminar(id: number) {
+  //   const opcion = confirm('¿Esta seguro que desea eliminar?');
+  //   if (opcion) {
+  //     this.clienteService.delete(id).subscribe((data) => {
+  //       alert('Registro eliminado');
+  //       this.formularioPersona.reset();
+  //     });
+  //   } else {
+  //   }
+  // }
 }
