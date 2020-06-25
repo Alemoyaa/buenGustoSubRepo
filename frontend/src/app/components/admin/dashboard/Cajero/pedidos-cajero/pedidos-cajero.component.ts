cccf0701c70720219eb02e2ef12b85dcd35ef442
ramiro2021 <ramiroamurua@hotmail.com>
2020-06-24 22:11:13 -0300

    confirmacion de modificacion de estado

--- frontend/src/app/components/admin/dashboard/Cajero/pedidos-cajero/pedidos-cajero.component.ts
+++ frontend/src/app/components/admin/dashboard/Cajero/pedidos-cajero/pedidos-cajero.component.ts
@@ -5,7 +5,7 @@ import { PedidoServices } from '../../../../../services/serviciosCliente/pedidoS
 import { filter } from 'rxjs/operators';
 import { AlertsService } from 'src/app/services/alertServices/alerts.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pedidos-cajero',
@@ -73,27 +73,53 @@ export class PedidosCajeroComponent implements OnInit {
     console.log(pedido);
     this.pedidoSeleccionado = pedido;
     this.estadoPedidoService.getOne(id).subscribe(estado => {
-      console.log(estado);
-      this.formularioEstado.setValue({
-        id: estado.id,
-        nombreEstado: estado.nombreEstado
-      });
-      console.log(this.formularioEstado.value);
 
-      this.pedidoSeleccionado.estadoPedido.id = this.formularioEstado.value.id;
-      this.pedidoSeleccionado.estadoPedido.nombreEstado = this.formularioEstado.value.nombreEstado;
-      
-      this.pedidoService.put(this.pedidoSeleccionado.id,this.pedidoSeleccionado ).subscribe(
-        data=> {
-          console.log(data);
+      Swal.fire({
+        title: `Realmente desea cambiar el estado del pedido a ${estado.nombreEstado}?`,
+        text: "Recuerda que una vez asignado un nuevo estado, no podras volver a cambiarlo!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, modificar estado',
+        cancelButtonText: 'Cancelar modificacion'
+      }).then((result) => {
+        if (result.value) {
+
+
+          console.log(estado);
+          this.formularioEstado.setValue({
+            id: estado.id,
+            nombreEstado: estado.nombreEstado
+          });
+          console.log(this.formularioEstado.value);
+
+          this.pedidoSeleccionado.estadoPedido.id = this.formularioEstado.value.id;
+          this.pedidoSeleccionado.estadoPedido.nombreEstado = this.formularioEstado.value.nombreEstado;
+
+          // this.pedidoService.put(this.pedidoSeleccionado.id, this.pedidoSeleccionado).subscribe();
+          console.log(this.pedidoSeleccionado);
+          
+          Swal.fire(
+            `El estado de su pedido fue modificado correctamente a: ${estado.nombreEstado}`,
+            'Puedes continuar con mas pedidos en la seccion de Estados de Pedidos!',
+            'success'
+          );
+
+          this.formularioEstado.reset();
+        }else{
+          this.formularioEstado.reset();
         }
-      );
-      console.log(this.pedidoSeleccionado);
+      });
+
+
+
+
     });
 
-    
-    this.alerts.mensajeWarning('Desea cambiar el estado del pedido?', 'Al cambiar el estado del pedido usted lo enviara a la cocina y no tendra acceso a el')
+
+
   }
 
-  
+
 }
