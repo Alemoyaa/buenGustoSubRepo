921357202cfad70fbde39feb56338bbc1c0a1c60
ramiro2021 <ramiroamurua@hotmail.com>
2020-06-25 03:26:01 -0300

    funciona todo, solo falta cambiar el estado del pedido a facturado

--- frontend/src/app/components/admin/dashboard/Cajero/facturacion-cajero/facturacion-cajero.component.ts
+++ frontend/src/app/components/admin/dashboard/Cajero/facturacion-cajero/facturacion-cajero.component.ts
@@ -55,7 +55,7 @@ export class FacturacionCajeroComponent implements OnInit {
       }
 
     );
-
+ 
   }
 
   mostrarFactura(factura){
--- frontend/src/app/components/admin/dashboard/Cajero/facturacion-cajero/modal-factura/modal-factura.component.ts
+++ frontend/src/app/components/admin/dashboard/Cajero/facturacion-cajero/modal-factura/modal-factura.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Factura } from 'src/app/entidades/Factura';
+import html2canvas from 'html2canvas';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-modal-factura',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal-factura.component.css']
 })
 export class ModalFacturaComponent implements OnInit {
-
+  factura: Factura;
+  @Input() set facturaProp( factura ){
+    if(factura){
+      this.factura = factura;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
+  
 
+  crearFacturaPDF() {
+    var data = document.getElementById('facturaPDF');
+
+    html2canvas(data).then((canvas) => {
+      // Configuracion necesaria para la foto
+      var imgWidth = 208;
+      var imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      const contentDataURL = canvas.toDataURL('image/png'); //Creamos una imagen la cual despues convertimos
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 va a ser el tamaño del PDF
+      var position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('factura.pdf'); // Generamos el PDF
+    });
+  }
+
+  
 }
