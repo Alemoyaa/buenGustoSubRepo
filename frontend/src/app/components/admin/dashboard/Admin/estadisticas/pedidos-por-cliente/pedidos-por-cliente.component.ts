62e8fe8b3d1db0fe9cfdb77f8573dd6aa90c16f5
alemoya <alejandro5320@gmail.com>
2020-06-28 20:10:04 -0300

    fix

--- frontend/src/app/components/admin/dashboard/Admin/estadisticas/pedidos-por-cliente/pedidos-por-cliente.component.ts
+++ frontend/src/app/components/admin/dashboard/Admin/estadisticas/pedidos-por-cliente/pedidos-por-cliente.component.ts
@@ -40,8 +40,8 @@ export class PedidosPorClienteComponent implements OnInit {
   }
 
   get filtrar(): Cliente[] {
-    var matcher = new RegExp(this.filtroBuscador, 'i');
-    return this.clientesEncontrados.filter(function (cliente) {
+    const matcher = new RegExp(this.filtroBuscador, 'i');
+    return this.clientesEncontrados.filter((cliente) => {
       return matcher.test([cliente.nombre, cliente.apellido].join());
     });
   }
