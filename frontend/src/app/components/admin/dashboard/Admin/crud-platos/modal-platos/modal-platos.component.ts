a091839542ea8b9fc49e9087fc18c8a70ac0ebeb
ramiro2021 <ramiroamurua@hotmail.com>
2020-06-29 03:29:28 -0300

    si arreglamos el setValue() tenemos platos

--- frontend/src/app/components/admin/dashboard/Admin/crud-platos/modal-platos/modal-platos.component.ts
+++ frontend/src/app/components/admin/dashboard/Admin/crud-platos/modal-platos/modal-platos.component.ts
@@ -24,9 +24,11 @@ export class ModalPlatosComponent implements OnInit {
   rubroGeneral: RubroGeneral[];
 
   esEditar: boolean = false;
+
+  //formulario
   formularioArticulo: FormGroup;
-  articuloActualizar: ArticuloManufacturado;
-  id: number;
+  // articuloActualizar: ArticuloManufacturado;
+  // id: number;
   constructor(
     private fb: FormBuilder,
     private serviceArtManufac: ArticuloManufacturadoService,
@@ -60,12 +62,59 @@ export class ModalPlatosComponent implements OnInit {
   }
 
   // rubro: RubroGeneral[];
-  traerRubrosGenerales(){
+  traerRubrosGenerales() {
     this.serviceRubroGeneral.getAll().subscribe(rubro => {
       this.rubroGeneral = rubro;
     });
   }
 
+  // (change)="seleccionarRubro($event.target.value)"
+
+  seleccionarRubro(id: number) {
+    this.serviceRubroGeneral.getOne(id).subscribe(rubro => {
+      this.formularioArticulo.controls.rubro.setValue({
+        id: rubro.id
+      });
+    });
+  }
+
+  seleccionarUnidadDeMedida(id: number) {
+    console.log(id);
+
+    const control = <FormArray>this.formularioArticulo.controls['lista_detalleManufacturado'];
+    const controlunidad = <FormGroup>control.controls['UnidadMedidaID'];
+
+    this.serviceUnidadDeMedida.getOne(id).subscribe(unidad => {
+
+      console.log(unidad);
+      console.log(control.value);
+      console.log(controlunidad.value);
+
+      controlunidad.setValue({
+        id: unidad.id,
+
+      });
+      console.log(this.formularioArticulo.value);
+    });
+
+
+  }
+
+  seleccionarIngrediente(id: number) {
+    console.log(id);
+    const control = <FormArray>this.formularioArticulo.controls['lista_detalleManufacturado']
+
+    const controlarticulo = <FormGroup>control.controls['articuloInsumoID'];
+    this.serviceArticuloInsumo.getOne(id).subscribe(articulo => {
+ 
+      controlarticulo.setValue({
+        id: articulo.id
+      });
+      console.log(this.formularioArticulo.value);
+    });
+  }
+
+  // seleccionar
 
   crearFormulario() {
     this.formularioArticulo = new FormGroup({
@@ -87,43 +136,22 @@ export class ModalPlatosComponent implements OnInit {
   }
 
   agregarIngrediente() {
-    const ingredienteNuevo = this.fb.group({
-      cantidad: 0,
-      UnidadMedidaID: this.fb.group({
-        id: 0,
+    const ingredienteNuevo = new FormGroup({
+      cantidad: new FormControl(0),
+      UnidadMedidaID: new FormGroup({
+        id: new FormControl(0),
+
       }),
-      articuloInsumoID: this.fb.group({
-        id: 0
+      articuloInsumoID: new FormGroup({
+        id: new FormControl(0),
+
       })
     });
 
     this.lista_detalleManufacturado.push(ingredienteNuevo);
   }
 
-  actualizar() {
-    this.serviceArtManufac
-      .put(this.id, this.formularioArticulo.value)
-      .subscribe(
-        (res) => {
-
-          this.alerts.mensajeSuccess(
-            'Actualizacion realizada',
-            `El articulo ${this.articuloActualizar.denominacion} se actualizo correctamente, recuerde que puede modificarlo cuando usted lo desee`
-          );
-          console.log(this.id);
-
-          this.esEditar = false;
-          this.formularioArticulo.reset();
-        },
-        (err) => {
-          this.alerts.mensajeError(
-            'No se ah podido actualizar el Rol del usuario',
-            'ah ocurrido un error y no se ah podido realizar la actualizacio, porfavor verifique que esten todos los datos correctos'
-          );
-        }
-      );
-  }
-
+ 
 
 
   cerrar() {
@@ -131,7 +159,33 @@ export class ModalPlatosComponent implements OnInit {
     this.esEditar = false;
   }
 
-  // editar(articulo: ArticuloManufacturado) {
+
+  crear() {
+
+    console.log(this.formularioArticulo.value);
+    this.serviceArtManufac.post(this.formularioArticulo.value).subscribe(
+      (data) => {
+        // this.articuloActualizar = data;
+
+        this.formularioArticulo.reset();
+        Swal.fire('success', 'Articulo agregado ', 'success');
+      },
+      (err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ocurrio un problema',
+          html: 'Por favor vuelva a intentarlo mas tarde',
+        });
+        console.log(err);
+      }
+    );
+  }
+
+
+  removerDetalle(id: number) {
+    this.lista_detalleManufacturado.removeAt(id);
+  }
+// editar(articulo: ArticuloManufacturado) {
   //   console.log(articulo);
   //   this.esEditar = true;
   //   this.formularioArticulo.setValue({
@@ -146,31 +200,28 @@ export class ModalPlatosComponent implements OnInit {
   //   console.log(this.id);
   // }
 
+   // actualizar() {
+  //   this.serviceArtManufac
+  //     .put(this.id, this.formularioArticulo.value)
+  //     .subscribe(
+  //       (res) => {
 
-  crear() {
+  //         this.alerts.mensajeSuccess(
+  //           'Actualizacion realizada',
+  //           `El articulo ${this.articuloActualizar.denominacion} se actualizo correctamente, recuerde que puede modificarlo cuando usted lo desee`
+  //         );
+  //         console.log(this.id);
 
-    console.log(this.formularioArticulo.value);
-    // this.serviceArtManufac.post(this.formularioArticulo.value).subscribe(
-    //   (data) => {
-    //     this.articuloActualizar = data;
-
-    //     this.formularioArticulo.reset();
-    //     Swal.fire('success', 'Articulo agregado ', 'success');
-    //   },
-    //   (err) => {
-    //     Swal.fire({
-    //       icon: 'error',
-    //       title: 'Ocurrio un problema',
-    //       html: 'Por favor vuelva a intentarlo mas tarde',
-    //     });
-    //     console.log(err);
-    //   }
-    // );
-  }
-
-
-  removerDetalle(id: number) {
-    this.lista_detalleManufacturado.removeAt(id);
-  }
+  //         this.esEditar = false;
+  //         this.formularioArticulo.reset();
+  //       },
+  //       (err) => {
+  //         this.alerts.mensajeError(
+  //           'No se ah podido actualizar el Rol del usuario',
+  //           'ah ocurrido un error y no se ah podido realizar la actualizacio, porfavor verifique que esten todos los datos correctos'
+  //         );
+  //       }
+  //     );
+  // }
 
 }
