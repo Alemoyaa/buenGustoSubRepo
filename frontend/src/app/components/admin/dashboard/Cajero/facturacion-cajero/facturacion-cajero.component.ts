65deb37fe4d3e182a547279126b6126ecd703fe6
alemoya <alejandro5320@gmail.com>
2020-07-11 13:27:50 -0300

    Estado en camino visual lista, falta cambiar estado

--- frontend/src/app/components/admin/dashboard/Cajero/facturacion-cajero/facturacion-cajero.component.ts
+++ frontend/src/app/components/admin/dashboard/Cajero/facturacion-cajero/facturacion-cajero.component.ts
@@ -4,10 +4,10 @@ import {Component, OnInit} from '@angular/core';
 import {AlertsService} from 'src/app/services/alertServices/alerts.service';
 import {FacturaService} from '../../../../../services/serviciosCliente/facturaServices/factura.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {Pedido} from '../../../../../entidades/Pedido';
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 import {EstadoPedidoServices} from '../../../../../services/serviciosCliente/estadoPedidoServices/estadoPedido.service';
 import {PedidoServices} from '../../../../../services/serviciosCliente/pedidoServices/pedido.service';
+import {EstadoPedido} from '../../../../../entidades/EstadoPedido';
 
 @Component({
   selector: 'app-facturacion-cajero',
@@ -57,34 +57,28 @@ export class FacturacionCajeroComponent implements OnInit {
 
   ngOnInit(): void {
     this.getPedidosPendientes();
-    this.crearFormulario();
   }
 
-  crearFormulario() {
-    this.formularioEstado = this.fb.group({
-      id: 0,
-      nombreEstado: '',
-    });
-  }
 
   getPedidosPendientes() {
-    this.facturaService.getAll().subscribe((data) => {
-      data.filter((factura) => {
-        if (factura.pedidofacturado.estadoPedido.nombreEstado === 'Listo') {
-          return this.facturas.push(factura);
-        } else if (
-          factura.pedidofacturado.estadoPedido.nombreEstado === 'En camino'
-        ) {
-          console.log('Factura de pedido en camino');
-          this.facturas.push(factura);
-        }
-        // console.log(this.facturas);
-      });
-
-      //this.pedidos = data;
-    }, error => {
-      console.log(error);
-    });
+    this.facturaService.getAll().subscribe(
+      (data) => {
+        data.filter((factura) => {
+          if (factura.pedidofacturado.estadoPedido.nombreEstado === 'Listo') {
+            return this.facturas.push(factura);
+          } else if (
+            factura.pedidofacturado.estadoPedido.nombreEstado === 'En camino'
+          ) {
+            this.facturas.push(factura);
+          }
+          // console.log(this.facturas);
+        });
+        //this.pedidos = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   mostrarFactura(factura) {
@@ -92,53 +86,46 @@ export class FacturacionCajeroComponent implements OnInit {
     console.log(this.factura);
   }
 
-  seleccionarEstado(id: number, factura: Factura) {
-    console.log(id);
-    console.log(factura);
-    this.facturaSeleccionada = factura;
-    this.estadoPedidoService.getOne(id).subscribe((estado) => {
-      Swal.fire({
-        title: `Realmente desea cambiar el estado del pedido a ${estado.nombreEstado}?`,
+  enviarADomicilio(factura: Factura, iterador: number) {
+    const estadoPedido = new EstadoPedido();
+    estadoPedido.id = 5;
+    estadoPedido.nombreEstado = 'En camino';
+    Swal.fire({
+        title: `Realmente desea cambiar el estado del pedido a 'En camino'?`,
         text:
-          'Recuerda que una vez asignado un nuevo estado, no podras volver a cambiarlo!',
+          'Recuerda que una vez asignado un nuevo estado, no podrás volver a cambiarlo!',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Si, modificar estado',
-        cancelButtonText: 'Cancelar modificacion',
+        cancelButtonText: 'Cancelar modificación',
       }).then((result) => {
         if (result.value) {
-          console.log(estado);
-          this.formularioEstado.setValue({
-            id: estado.id,
-            nombreEstado: estado.nombreEstado,
-          });
-          console.log(this.formularioEstado.value);
-
-          this.facturaSeleccionada.pedidofacturado.estadoPedido.id = this.formularioEstado.value.id;
-          this.facturaSeleccionada.pedidofacturado.estadoPedido.nombreEstado = this.formularioEstado.value.nombreEstado;
-
-          this.pedidoService
-            .editarEstadoPedido(
-              this.facturaSeleccionada.id,
-              this.facturaSeleccionada.pedidofacturado.estadoPedido
-            )
-            .subscribe();
-          console.log(this.facturaSeleccionada);
-
-          Swal.fire(
-            `El estado de su pedido fue modificado correctamente a: ${estado.nombreEstado}`,
-            'Puedes continuar con mas pedidos en la seccion de Estados de Pedidos!',
-            'success'
+          this.pedidoService.editarEstadoPedido(
+            factura.id,
+            estadoPedido
+          ).subscribe(
+            (res) => {
+              console.log('estado enviado', estadoPedido);
+              console.log('respuesta backend', res);
+              this.facturas[iterador].pedidofacturado = res;
+              Swal.fire(
+                `El estado de su pedido fue modificado correctamente  a 'En camino'`,
+                'Puedes continuar con mas pedidos en la sección de Estados de Pedidos!',
+                'success'
+              );
+            }, (error) => {
+              console.log(error);
+            }
           );
-          this.getPedidosPendientes();
-          this.formularioEstado.reset();
         } else {
-          this.formularioEstado.reset();
+          Swal.fire(
+            `Estado no editado`,
+            '',
+            'info'
+          );
         }
       });
-    });
   }
-
 }
