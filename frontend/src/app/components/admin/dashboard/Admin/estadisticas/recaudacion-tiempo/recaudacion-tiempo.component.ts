4ca6f12eb08e2055025d9d41c0d808f40c88900b
alemoya <alejandro5320@gmail.com>
2020-07-24 17:01:41 -0300

    RecaudacionTiempo limpio

--- frontend/src/app/components/admin/dashboard/Admin/estadisticas/recaudacion-tiempo/recaudacion-tiempo.component.ts
+++ frontend/src/app/components/admin/dashboard/Admin/estadisticas/recaudacion-tiempo/recaudacion-tiempo.component.ts
@@ -2,7 +2,6 @@ import { PedidoExcel } from './../../../../../../services/excelServices/entidade
 import { ExcelService } from './../../../../../../services/excelServices/excel.service';
 import { Pedido } from './../../../../../../entidades/Pedido';
 import { PedidoServices } from './../../../../../../services/serviciosCliente/pedidoServices/pedido.service';
-import { Chart } from 'node_modules/chart.js';
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { NgForm } from '@angular/forms';
@@ -83,7 +82,6 @@ export class RecaudacionTiempoComponent implements OnInit {
       this.pedidoParaExcel.hora_estimada_fin = this.listaDePedidos[
         key
       ].hora_estimada_fin;
-      this.pedidoParaExcel.minutosTotal = this.listaDePedidos[key].minutosTotal;
       this.pedidoParaExcel.numero = this.listaDePedidos[key].numero;
       this.pedidoParaExcel.tipo_Envio = this.listaDePedidos[key].tipo_Envio;
       this.pedidoParaExcel.totalPedido = this.listaDePedidos[key].totalPedido;
@@ -101,49 +99,5 @@ export class RecaudacionTiempoComponent implements OnInit {
       this.listaPedidosParaExcel,
       'Recaudacion por tiempo'
     );
-  }
-
-  newChart(nombre, labels, data) {
-    var ctx = document.getElementById(nombre);
-    var myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: '#',
-            data: data,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
   }
 }
